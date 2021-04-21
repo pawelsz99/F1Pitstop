ba51b83f5333456163d587ba6f91ffbf677e4b43
Pawel <52848605+pawelsz99@users.noreply.github.com>
2021-04-21 14:21:21 +0200

    back button added
    but the titles for the fragments doesn't work

--- app/src/main/java/com/pawelsznuradev/f1pitstop/MainActivity.kt
+++ app/src/main/java/com/pawelsznuradev/f1pitstop/MainActivity.kt
@@ -2,17 +2,11 @@ package com.pawelsznuradev.f1pitstop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.pawelsznuradev.f1pitstop.databinding.ActivityMainBinding
-import com.pawelsznuradev.f1pitstop.network.ErgastApi
-import com.pawelsznuradev.f1pitstop.network.ResponseDrivers
-import com.pawelsznuradev.f1pitstop.network.ResponsePitStops
-import com.pawelsznuradev.f1pitstop.network.ResponseRaces
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
  * Main Activity and entry point for the app.
@@ -20,12 +14,27 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
     }
 
+    /**
+     * Enables back button support. Simply navigates one element up on the stack.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
